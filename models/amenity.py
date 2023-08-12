837e8763728694384fb89e80c0b8671fbd28f76d
skfada <adamusolomon44@gmail.com>
2023-08-12 21:40:15 +0100

    ./models/amenity.py

---
+++ models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Defienes ameenities
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Defienes ameniteies theat useer cean choose to offer at its place"""
+    name = ""
