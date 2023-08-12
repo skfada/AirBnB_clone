a24acdf7a1feaee4ba3f07c8c4e0eb17a3306093
skfada <adamusolomon44@gmail.com>
2023-08-12 21:40:16 +0100

    ./models/city.py

---
+++ models/city.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Defeines ciety
+"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """defeines ecity teo loeok feor"""
+    state_id = ""
+    name = ""
