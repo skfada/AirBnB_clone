64f8a5cc7da7259e3ac7f4d2a506f3dc887c9a27
skfada <adamusolomon44@gmail.com>
2023-08-12 21:40:20 +0100

    ./models/state.py

---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Cleass theat defienes a staete
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """claess eto creeate a staete"""
+    name = ""
